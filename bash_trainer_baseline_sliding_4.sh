b31be14116380207007bf1d20fabb5ed3dc78e4a
“jiangjin1999” <1261842974@qq.com>
2023-05-22 13:46:00 +0800

    2023.5.22commit

--- bash_trainer_baseline_sliding_4.sh
+++ bash_trainer_baseline_sliding_4.sh
@@ -7,16 +7,16 @@
 #         --test_batch_size 80 \
 #         --is_sliding_4 \
 #         --max_seq_length 40
-export CUDA_VISIBLE_DEVICES=0
-python model-trainer.py \
-        --current_dataset HKUST \
-        --train_batch_size 35 \
-        --dev_batch_size 35 \
-        --test_batch_size 35 \
-        --is_sliding_4 \
-        --max_seq_length 80
+# export CUDA_VISIBLE_DEVICES=0
+# python model-trainer.py \
+#         --current_dataset HKUST \
+#         --train_batch_size 35 \
+#         --dev_batch_size 35 \
+#         --test_batch_size 35 \
+#         --is_sliding_4 \
+#         --max_seq_length 80
 # 代码中已经全部设置为False,设置为True时，需要解除 注释
-# export CUDA_VISIBLE_DEVICES=3
+# export CUDA_VISIBLE_DEVICES=2
 # python model-trainer.py \
 #         --current_dataset LIBRISPEECH_CLEAN \
 #         --train_batch_size 35 \
@@ -25,11 +25,11 @@ python model-trainer.py \
 #         --is_sliding_4 \
 #         --max_seq_length 100
 # 代码中已经全部设置为False,设置为True时，需要解除 注释
-# export CUDA_VISIBLE_DEVICES=3
-# python model-trainer.py \
-#         --current_dataset LIBRISPEECH_OTHER\
-#         --train_batch_size 35 \
-#         --dev_batch_size 35 \
-#         --test_batch_size 35 \
-#         --is_sliding_4 \
-#         --max_seq_length 100
+export CUDA_VISIBLE_DEVICES=3
+python model-trainer.py \
+        --current_dataset LIBRISPEECH_OTHER\
+        --train_batch_size 35 \
+        --dev_batch_size 35 \
+        --test_batch_size 35 \
+        --is_sliding_4 \
+        --max_seq_length 100
